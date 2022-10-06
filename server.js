91a7edbd6a3598476c969711bf3210228f43a427
oangel26 <oa.angel26@gmail.com>
2022-10-06 03:15:34 +0100

    Not my code. This is TEST

---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import express from 'express';
+
+const routes = require('./routes/index');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+app.use(express.json());
+app.use('/', routes);
+module.exports = app;
